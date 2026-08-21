a5fd90b26736f1dcdb87b32efeca720a8fe10d92
Kuglof01 <kendreis.12@gmail.com>
2026-08-21 15:06:56 +0200

    open next config

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,5 @@
 import type { NextConfig } from 'next';
 const nextConfig: NextConfig = { reactStrictMode: true };
 export default nextConfig;
+
+import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
